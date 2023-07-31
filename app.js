1ba4bd5be39a9a38adb94583ed0ad22df1e18b56
Gabriel Alejandro Mamani <gamcode98@gmail.com>
2023-07-31 11:04:10 -0300

    Clase 21: Trabajando con formularios

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/home.html'));
 })
 
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/register.html'));
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/login.html'));
+})
+
+app.post(['/login', '/signup'], (req, res) => { 
+  res.redirect('/');
+})
+
 app.use(express.static('public'))
 
 app.listen(PORT, () => { 
-    console.log(`Server running on: http://localhost:${PORT}`);
+  console.log(`Server running on: http://localhost:${PORT}`);
 })
