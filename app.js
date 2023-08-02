30645cff12bbbd9a3ecf2307edb1ecfa0a126219
Gabriel Alejandro Mamani <gamcode98@gmail.com>
2023-08-02 10:43:13 -0300

    add environment port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => { 
   res.sendFile(path.join(__dirname, '/views/home.html'));
